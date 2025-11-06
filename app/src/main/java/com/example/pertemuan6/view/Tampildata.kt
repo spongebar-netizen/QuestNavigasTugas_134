0593b492d8ca46feca80da7f9e2a479ac5782a9c
Lalu Fatan <lalufatan25@gmail.com>
2025-11-06 17:05:11 +0700

    nambahkan bebrapa column

--- app/src/main/java/com/example/pertemuan6/view/Tampildata.kt
+++ app/src/main/java/com/example/pertemuan6/view/Tampildata.kt
@@ -50,4 +50,34 @@ fun TampilData(
                         (id = R.color.teal_700)
                     )
             )
-       
+        }){ isiRuang->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            Column (
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource
+                    (id = R.dimen.padding_small))
+            ){
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(),
+                            fontSize = 16.sp)
+                        Text(text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick
+                ) {
+                    Text(text = stringResource(id=R.string.back))
+                }
+            }
+        }
+    }
+}
