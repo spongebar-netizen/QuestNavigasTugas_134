1f096d2da730772837332a63d802d83e07d70d05
Lalu Fatan <lalufatan25@gmail.com>
2025-11-06 16:00:32 +0700

    menambahkan colum outlinetextfield

--- app/src/main/java/com/example/pertemuan6/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/Formulir.kt
@@ -1,2 +1,57 @@
 package com.example.pertemuan6
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.res.colorResource
+import androidx.compose.material3.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+
+
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold (modifier= Modifier,
+        topBar= {
+            TopAppBar(
+                title = {Text(text=stringResource(id= R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (containerColor = colorResource(id = R.color.teal_700))
+            )
+        }
+    ){isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues=isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top=20.dp)
+                    .width(width=250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+            HorizontalDivider(modifier = Modifier)
+
+        }
+
+    }
+}
