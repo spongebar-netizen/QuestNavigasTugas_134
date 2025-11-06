b22e09fc7312e6776ffb3ad6b8aae2db9b0b2cfd
Lalu Fatan <lalufatan25@gmail.com>
2025-11-06 17:02:15 +0700

    memperbaiki r

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.library.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
